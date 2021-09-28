a2122abb2ff985fcb4b2698fc0a733181be1c09d
Daniel Nasc <danielnasc15987@gmail.com>
2021-09-28 13:47:26 -0300

    final code (i hope)

--- README.md
+++ README.md
@@ -10,6 +10,3 @@ SECRET_KEY="your secret key for user data encryption"
 ```
 
 Visit: <a href="https://firebase.google.com/docs/firestore/quickstart">Firebase quickstart</a>
-
-## Why are client and server together in the same project?
-So I can upload them all at once to cs50, but I'm going to separate them into other branches
--- Server/services/encrypt.js
+++ Server/services/encrypt.js
@@ -1,10 +1,12 @@
 require("dotenv").config()
 const crypto = require("crypto")
-const cryptoJs = require("crypto-js")
 
 // https://dev.to/aditya278/understanding-and-implementing-password-hashing-in-nodejs-2m84
 // get secret key for user data encryption
+const algorithm = 'aes-256-cbc'
 const SECRET_KEY = process.env.SECRET_KEY
+const IV = process.env.IV
+
 function encrypt_Hmac(text)
 {
     const HASHED_STR = crypto.createHmac('sha256', SECRET_KEY)
@@ -13,17 +15,4 @@ function encrypt_Hmac(text)
     return HASHED_STR
 }
 
-function encrypt_AES(text)
-{
-    const ciphertext = cryptoJs.AES.encrypt(text, SECRET_KEY).toString()
-    return ciphertext
-}
-
-function decrypt_AES(ciphertext)
-{
-    const bytes = cryptoJs.AES.decrypt(ciphertext, SECRET_KEY)
-    const text = bytes.toString(cryptoJs.enc.Utf8)
-    return text
-}
-
-module.exports = {encrypt_Hmac, encrypt_AES, decrypt_AES}
+module.exports = {encrypt_Hmac}
